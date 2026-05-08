fe3426a23ad22f036f8422072e6bc87b5c4626b9
Tiger5286 <toratiger5286@gmail.com>
2026-05-08 09:55:41 +0900

    コメント追加

--- Project/Source/main.cpp
+++ Project/Source/main.cpp
@@ -4,12 +4,20 @@
 // プログラムは WinMain から始まる
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	// アプリケーションのインスタンスを生成
 	auto& app = Application::GetInstance();
+
+	// アプリケーションを初期化
 	if (!app.Init())
 	{
+		// 初期化に失敗したら終了
 		return -1;
 	}
+
+	// 実行
 	app.Run();
+
+	// 終了
 	app.Terminate();
 
 	return 0;
