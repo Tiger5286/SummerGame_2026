e9b8d7ced80012030a49c035cbee005dc4b3d06a
Tiger5286 <toratiger5286@gmail.com>
2026-05-18 14:27:23 +0900

    アニメータークラス改良

--- Project/Source/System/Animator.cpp
+++ Project/Source/System/Animator.cpp
@@ -20,6 +20,8 @@ void Animator::Init(int modelHandle, std::wstring animName)
 {
 	// モデルを設定
 	m_modelHandle = modelHandle;
+	//アニメーション名を設定
+	m_currentAnimName = animName;
 
 	// アニメーションをアタッチ
 	auto animIndex = MV1GetAnimIndex(m_modelHandle, animName.c_str());
@@ -55,10 +57,13 @@ void Animator::Update()
 	// アニメーションの進行
 	m_currentAnimTime += m_animSpeed;
 	// アニメーションのループ処理
-	float totalCount = MV1GetAttachAnimTotalTime(m_modelHandle, m_currentAnimHandle);
-	while (m_currentAnimTime >= totalCount)
+	if (m_isLoop)
 	{
-		m_currentAnimTime -= totalCount;
+		float totalCount = MV1GetAttachAnimTotalTime(m_modelHandle, m_currentAnimHandle);
+		while (m_currentAnimTime >= totalCount)
+		{
+			m_currentAnimTime -= totalCount;
+		}
 	}
 	MV1SetAttachAnimTime(m_modelHandle, m_currentAnimHandle, m_currentAnimTime);
 
@@ -75,7 +80,7 @@ void Animator::Update()
 	}
 }
 
-void Animator::ChangeAnim(std::wstring animName, float animSpeed)
+void Animator::ChangeAnim(std::wstring animName, float animSpeed,bool isLoop)
 {
 	// 新しくアニメーションを設定しようとしたとき
 	// 古いアニメーションが残っている場合、それをデタッチ
@@ -97,4 +102,42 @@ void Animator::ChangeAnim(std::wstring animName, float animSpeed)
 	m_animChangeFrame = 0;
 	// アニメーションの進行速度を設定
 	m_animSpeed = animSpeed;
+	// ループするかどうかを設定
+	m_isLoop = isLoop;
+	// アニメーション名を設定
+	m_currentAnimName = animName;
+}
+
+bool Animator::IsEnd()
+{
+	// ループするならfalseを返す
+	if (m_isLoop)
+	{
+		return false;
+	}
+
+	// アニメーションの総再生時間を現在の再生時間が下回っていたらfalse
+	auto animIndex = MV1GetAnimIndex(m_modelHandle, m_currentAnimName.c_str());
+	auto totalTime = MV1GetAnimTotalTime(m_modelHandle, animIndex);
+	if (m_currentAnimTime <= totalTime)
+	{
+		return false;
+	}
+
+	// 全てにあてはまらなければtrue
+	return true;
+}
+
+bool Animator::operator!=(const std::wstring& animName)
+{
+	return !(*this == animName);
+}
+
+bool Animator::operator==(const std::wstring& animName)
+{
+	if (m_currentAnimName == animName)
+	{
+		return true;
+	}
+	return false;
 }
--- Project/Source/System/Animator.h
+++ Project/Source/System/Animator.h
@@ -21,7 +21,17 @@ public:
 	/// </summary>
 	/// <param name="animName">次のアニメーション名</param>
 	/// <param name="animSpeed">アニメーションの再生速度</param>
-	void ChangeAnim(std::wstring animName, float animSpeed = 0.5f);
+	void ChangeAnim(std::wstring animName, float animSpeed = 0.5f,bool isLoop = true);
+
+	/// <summary>
+	/// 現在再生中のアニメーションが終わったかどうか取得する(ループが有効だとtrueを返さない)
+	/// </summary>
+	/// <returns>true:終わった / false:終わってない</returns>
+	bool IsEnd();
+
+	bool operator==(const std::wstring& animName);
+	bool operator!=(const std::wstring& animName);
+
 private:
 	// アニメーションを行うモデルのハンドル
 	int m_modelHandle = -1;
@@ -29,11 +39,15 @@ private:
 	// アニメーションのハンドル
 	int m_currentAnimHandle = -1;
 	int m_lastAnimHandle = -1;
+	// アニメーション名
+	std::wstring m_currentAnimName;
 
 	// アニメーション管理
 	float m_animSpeed = 0.5f;
 	float m_currentAnimTime = 0.0f;
 	float m_lastAnimTime = 0.0f;
+
+	bool m_isLoop = true;
 
 	// アニメーションの切り替えフレーム数
 	int m_animChangeFrame = 0;
