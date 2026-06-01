56ab9207f32f0ad74b9d1a6cf0fe17b2c20b99c1
Tiger5286 <toratiger5286@gmail.com>
2026-06-01 10:24:24 +0900

    PlayerのStateパターンを作ろうとしている

---
+++ Project/Source/Game/GameObject/Player/PlayerStateBase.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <memory>
+
+class Player;
+
+class PlayerStateBase
+{
+public:
+	PlayerStateBase() = default;
+	virtual ~PlayerStateBase() = default;
+
+	virtual void Enter(std::shared_ptr<Player> pPlayer) abstract;
+	virtual void Update() abstract;
+	virtual void Exit() abstract;
+
+	std::shared_ptr<PlayerStateBase> GetNextState()
+	{
+		return m_pNextState;
+	}
+
+	void ChangeState(std::shared_ptr<PlayerStateBase> pNextState)
+	{
+		m_pNextState = pNextState;
+	}
+
+protected:
+	std::shared_ptr<Player> m_pPlayer = nullptr;
+	std::shared_ptr<PlayerStateBase> m_pNextState = nullptr;
+};
+
