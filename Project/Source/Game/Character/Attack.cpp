53b5184783396035baa755e3ff741b59f1dc4656
Tiger5286 <toratiger5286@gmail.com>
2026-06-03 15:37:33 +0900

    Attackクラス作成

---
+++ Project/Source/Game/Character/Attack.cpp
@@ -0,0 +1,41 @@
+#include "Attack.h"
+#include "Game/Collider/SphereCollider.h"
+#include "Singleton/CollisionManager.h"
+
+void Attack::Init()
+{
+	m_pCollider = std::make_shared<SphereCollider>(m_data.colliderRadius);
+	CollisionManager::GetInstance().Register(shared_from_this());
+}
+
+void Attack::End()
+{}
+
+void Attack::Update()
+{
+	m_pCollider->SetPos(m_pos);
+}
+
+void Attack::Draw()
+{
+#ifdef _DEBUG
+	m_pCollider->Draw();
+#endif
+}
+
+void Attack::OnCollision(Character & other)
+{
+	// 同じIDがあるかどうか探す
+	auto it = std::find(m_hitIds.begin(), m_hitIds.end(), other.GetID());
+	// 同じIDがなかった(初めて当たった相手)の場合
+	if (it == m_hitIds.end())
+	{
+		// 当たった相手のIDを登録
+		m_hitIds.push_back(other.GetID());
+		// 当たった相手が設定された相手だったら
+		if (other.GetType() == m_data.hitCharacterType)
+		{
+			other.OnHitAttack();
+		}
+	}
+}
---
+++ Project/Source/Game/Character/Attack.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Character.h"
+#include <memory>
+#include <list>
+
+class Attack :
+    public Character, public std::enable_shared_from_this<Attack>
+{
+public:
+    struct AttackData
+    {
+        float colliderRadius = 0.0f;
+        Character::Type hitCharacterType = Character::Type::None;
+    };
+
+    Attack() = default;
+    ~Attack() = default;
+
+    void Init() override;
+    void End() override;
+    void Update() override;
+    void Draw() override;
+
+    void OnCollision(Character& other) override;
+
+    /// <summary>
+    /// 攻撃の情報を設定する。Initの前に実行する。
+    /// </summary>
+    /// <param name="data"></param>
+    void SetData(AttackData data) { m_data = data; }
+
+private:
+    std::list<int> m_hitIds;
+    AttackData m_data;
+};
+
