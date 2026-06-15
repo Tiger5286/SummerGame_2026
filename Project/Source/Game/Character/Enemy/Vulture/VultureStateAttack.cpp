b41b08932b98ed5d34f8cb7daa09de0d8aca41f7
Tiger5286 <toratiger5286@gmail.com>
2026-06-15 18:39:57 +0900

    VultureStateAttack実装

--- Project/Source/Game/Character/Enemy/Vulture/VultureStateAttack.cpp
+++ Project/Source/Game/Character/Enemy/Vulture/VultureStateAttack.cpp
@@ -1,11 +1,16 @@
 #include "VultureStateAttack.h"
 #include "Vulture.h"
 #include "../../Player/Player.h"
+#include "Utility/MyLib.h"
+#include "../../Attack.h"
 
 #include "VultureStateIdle.h"
 
 namespace
 {
+	// アニメーション名
+	const std::wstring kAnimName = L"VultureCinereous_Skelmesh|VultureCinereous_Gliding";
+
 	// 降下にかかるフレーム数
 	constexpr int kDescentFrame = 60;
 	// 突進し始めるフレーム数
@@ -14,7 +19,15 @@ namespace
 	constexpr int kEndChargeFrame = kStartChargeFrame + 45;
 
 	// 突進のスピード
-	constexpr float kChargeSpeed = 10.0f;
+	constexpr float kChargeSpeed = 20.0f;
+
+	// 攻撃の情報
+	const MyLib::AttackData kAttackData = {
+		.colliderRadius = 80.0f,
+		.damage = 20,
+		.hitCharacterType = MyLib::CharacterType::Player,
+		.isKnockDown = false
+	};
 }
 
 void VultureStateAttack::Enter(std::weak_ptr<Vulture> pVulture)
@@ -22,7 +35,7 @@ void VultureStateAttack::Enter(std::weak_ptr<Vulture> pVulture)
 	m_pVulture = pVulture;
 	auto vulture = m_pVulture.lock();
 
-	m_toPlayerHeightDif = vulture->m_pos.y - vulture->m_pPlayer->GetPos().y;
+	m_toPlayerHeightDif = vulture->m_pos.y - (vulture->m_pPlayer->GetPos().y + 50.0f);
 }
 
 void VultureStateAttack::Update()
@@ -33,17 +46,39 @@ void VultureStateAttack::Update()
 	// 下降
 	if (m_frame < kDescentFrame)
 	{
-		vulture->m_flyHeight = vulture->m_pPlayer->GetPos().y;
-		vulture->m_vel.y = abs(m_toPlayerHeightDif) / kDescentFrame;
+		vulture->m_flyHeight = vulture->m_pPlayer->GetPos().y + 50.0f;
+		vulture->m_pos.y -= abs(m_toPlayerHeightDif) / kDescentFrame;
+	}
+	// 突進待機
+	if (m_frame < kStartChargeFrame)
+	{
+		// 突進するまでプレイヤーの方向を向く
+		m_toPlayerDir = (vulture->m_pPlayer->GetPos() - vulture->m_pos).Normalized();
+		// プレイヤーへの方向を向く
+		vulture->m_angle = atan2f(m_toPlayerDir.z, -m_toPlayerDir.x) + DX_PI_F / 2;
 	}
 	// 突進
 	if (m_frame == kStartChargeFrame)
-	{	// 突進開始時にプレイヤーへの方向を覚える
-		m_toPlayerDir = (vulture->m_pPlayer->GetPos() - vulture->m_pos).Normalized();
+	{
+		// 攻撃を生成
+		m_pAtk = std::make_shared<Attack>();
+		m_pAtk->SetData(kAttackData);
+		m_pAtk->Init();
+		m_pAtk->SetPos(vulture->m_pos + Vector3(0,50,0));
+		// アニメーションを変更
+		vulture->m_anim.ChangeAnim(kAnimName);
 	}
+	// 突進中
 	if (m_frame > kStartChargeFrame && m_frame < kEndChargeFrame)
 	{	// プレイヤーの方向に突進
 		vulture->m_vel = m_toPlayerDir * kChargeSpeed;
+		// 攻撃の位置を設定
+		m_pAtk->SetPos(vulture->m_pos + Vector3(0, 50, 0));
+	}
+	// 攻撃の更新
+	if (m_pAtk != nullptr)
+	{
+		m_pAtk->Update();
 	}
 
 	// 突進が終わったらidle
@@ -57,4 +92,14 @@ void VultureStateAttack::Update()
 void VultureStateAttack::Exit()
 {
 	m_pVulture.lock()->m_flyHeight = 300.0f;
+}
+
+void VultureStateAttack::Draw()
+{
+#ifdef _DEBUG
+	if (m_pAtk != nullptr)
+	{
+		m_pAtk->Draw();
+	}
+#endif
 }
--- Project/Source/Game/Character/Enemy/Vulture/VultureStateAttack.h
+++ Project/Source/Game/Character/Enemy/Vulture/VultureStateAttack.h
@@ -2,6 +2,8 @@
 #include "VultureStateBase.h"
 #include "Utility/Vector3.h"
 
+class Attack;
+
 class VultureStateAttack :
     public VultureStateBase
 {
@@ -12,9 +14,11 @@ public:
     void Enter(std::weak_ptr<Vulture> pVulture) override;
     void Update() override;
     void Exit() override;
+    void Draw() override;
 private:
     int m_frame = 0;
     float m_toPlayerHeightDif = 0.0f;
     Vector3 m_toPlayerDir;
+    std::shared_ptr<Attack> m_pAtk = nullptr;
 };
 
