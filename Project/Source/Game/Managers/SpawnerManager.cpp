438a891dc05c4c86ab45fa04b636593d8a45f579
Tiger5286 <toratiger5286@gmail.com>
2026-06-24 18:53:16 +0900

    SpawnerManagerのcsv読み込み実装中

--- Project/Source/Game/Managers/SpawnerManager.cpp
+++ Project/Source/Game/Managers/SpawnerManager.cpp
@@ -1,6 +1,9 @@
 #include "SpawnerManager.h"
 #include "../EnemySpawner.h"
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <cassert>
 
 namespace
 {
@@ -9,7 +12,35 @@ namespace
 
 void SpawnerManager::Load()
 {
+	// ファイルを開く
+	std::ifstream file(kfilePath);
+	// ファイルが開けなかったらassert
+	if (!file.is_open())
+	{
+		assert(false && "SpawnerData.csv が正しく開けませんでした");
+		return;
+	}
 
+	std::string line;
+	// 行単位で読み込む
+	while (std::getline(file, line))
+	{
+		// 行が空、または行の1文字目が#ならその行は無視する
+		if (line.empty() || line[0] == '#') continue;
+
+		// 新しいEnemySpawnerを生成する
+		m_pEnemySpawners.push_back(std::make_shared<EnemySpawner>());
+		auto spawner = m_pEnemySpawners.back();
+
+		std::stringstream ss(line);
+		std::string cell;
+		// カンマ区切りでセルごとに分解する
+		while (std::getline(ss, cell, ','))
+		{
+			
+		}
+		// 行の終わり
+	}
 }
 
 void SpawnerManager::Update()
