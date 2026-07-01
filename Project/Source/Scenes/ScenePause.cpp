794b0737e4f62cccbf415dd444e6bb567fac8476
Tiger5286 <toratiger5286@gmail.com>
2026-07-01 11:14:19 +0900

    ポーズメニューを作成

--- Project/Source/Scenes/ScenePause.cpp
+++ Project/Source/Scenes/ScenePause.cpp
@@ -3,11 +3,26 @@
 #include "Game.h"
 #include "Singleton/Input.h"
 #include "SceneManager.h"
+#include <map>
+
+#include "SceneTitle.h"
 
 ScenePause::ScenePause(SceneManager& sceneManager) :
 	SceneBase(sceneManager)
 {
-
+	// メニューのアクションを設定する
+	m_menuActions[static_cast<int>(PauseMenu::Resume)] = {
+		.name = L"Resume",
+		.action = [this]() { Resume(); }
+	};
+	m_menuActions[static_cast<int>(PauseMenu::Option)] = {
+		.name = L"Option",
+		.action = [this]() { Option(); }
+	};
+	m_menuActions[static_cast<int>(PauseMenu::BackToTitle)] = {
+		.name = L"Back to Title",
+		.action = [this]() { BackToTitle(); }
+	};
 }
 
 void ScenePause::Init()
@@ -23,9 +38,34 @@ void ScenePause::End()
 void ScenePause::Update()
 {
 	auto& input = Input::GetInstance();
+
+	// 上下キーでメニューの選択を変更する
+	if (input.IsTriggerd(XINPUT_BUTTON_DPAD_DOWN))
+	{
+		m_selectIndex++;
+		if (m_selectIndex >= static_cast<int>(PauseMenu::Num))
+		{
+			m_selectIndex = 0;
+		}
+	}
+	if (input.IsTriggerd(XINPUT_BUTTON_DPAD_UP))
+	{
+		m_selectIndex--;
+		if (m_selectIndex < 0)
+		{
+			m_selectIndex = static_cast<int>(PauseMenu::Num) - 1;
+		}
+	}
+	// 決定ボタンで選択中のメニューのアクションを実行する
+	if (input.IsTriggerd(XINPUT_BUTTON_A))
+	{
+		m_menuActions[m_selectIndex].action();
+		return;
+	}
+	// スタートボタンでポーズを解除する
 	if (input.IsTriggerd(XINPUT_BUTTON_START))
 	{
-		m_sceneManager.PopScene();
+		Resume();
 		return;
 	}
 }
@@ -36,7 +76,29 @@ void ScenePause::Draw()
 	DrawBox(0, 0, Game::kScreenWidth, Game::kScreenHeight, 0x000000, true);
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
+	for (int i = 0; i < static_cast<int>(PauseMenu::Num); i++)
+	{
+		unsigned int color = 0xffffff;
+		if (i == m_selectIndex) color = 0xff0000;
+		DrawString(100, 100 + i * 20, m_menuActions[i].name.c_str(), color);
+	}
+
 #ifdef _DEBUG
 	DrawString(0, 0, L"ScenePause", 0xffffff);
 #endif
+}
+
+void ScenePause::Resume()
+{
+	m_sceneManager.PopScene();
+}
+
+void ScenePause::Option()
+{
+	
+}
+
+void ScenePause::BackToTitle()
+{
+	m_sceneManager.ResetScene(std::make_shared<SceneTitle>(m_sceneManager));
 }
--- Project/Source/Scenes/ScenePause.h
+++ Project/Source/Scenes/ScenePause.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "SceneBase.h"
+#include <functional>
+#include <string>
+#include <array>
+
 class ScenePause :
     public SceneBase
 {
@@ -13,6 +17,29 @@ public:
 	void Draw() override;
 
 private:
+	void Resume();
+	void Option();
+	void BackToTitle();
+
+	enum class PauseMenu
+	{
+		Resume,
+		Option,
+		BackToTitle,
+
+		Num
+	};
+
+	struct MenuItem
+	{
+		std::wstring name;
+		std::function<void()> action;
+	};
+
+private:
+	int m_selectIndex = 0;	// 選択中のメニューのインデックス
+	// メニューのアクション(関数)を格納する配列
+	std::array<MenuItem, static_cast<int>(PauseMenu::Num)> m_menuActions;
 
 };
 
