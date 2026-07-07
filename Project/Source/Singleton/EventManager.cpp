4e26654e064fe290bcb2acc2f880bb89bd778f89
Tiger5286 <toratiger5286@gmail.com>
2026-07-07 11:20:31 +0900

    EventManager作成

---
+++ Project/Source/Singleton/EventManager.cpp
@@ -0,0 +1,38 @@
+﻿#include "EventManager.h"
+
+void EventManager::CallEvent(const std::string& eventName)
+{
+	for (auto& data : m_eventDatas)
+	{
+		if (data.eventName == eventName)
+		{
+			data.func();
+		}
+	}
+}
+
+int EventManager::Register(const std::string& eventName, std::function<void()> func)
+{
+	m_newHandle++;
+	EventData newData = {
+		.handle = m_newHandle,
+		.eventName = eventName,
+		.func = func
+	};
+	m_eventDatas.push_back(newData);
+	return newData.handle;
+}
+
+void EventManager::UnRegister(int handle)
+{
+	EventData removeData;
+	for (auto& data : m_eventDatas)
+	{
+		if (data.handle == handle)
+		{
+			removeData = data;
+			break;
+		}
+	}
+	m_eventDatas.remove(removeData);
+}
---
+++ Project/Source/Singleton/EventManager.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <string>
+#include <functional>
+#include <list>
+
+class EventManager
+{
+public:
+	static EventManager& GetInstance();
+	~EventManager() = default;
+
+	EventManager(const EventManager&) = delete;
+	EventManager& operator=(const EventManager&) = delete;
+private:
+	EventManager() = default;
+
+	struct EventData
+	{
+		int handle = -1;
+		std::string eventName;
+		std::function<void()> func;
+
+		bool operator==(const EventData& data) const
+		{
+			return handle == data.handle;
+		}
+	};
+public:
+
+	/// <summary>
+	/// 登録したイベントを発火させる。
+	/// </summary>
+	/// <param name="eventName">発火させるイベント名</param>
+	void CallEvent(const std::string& eventName);
+
+	/// <summary>
+	/// イベント発火時に呼ばれる関数を登録する。
+	/// </summary>
+	/// <param name="eventName">イベント名</param>
+	/// <param name="func">発火時に呼ばれる関数</param>
+	/// <returns>管理番号</returns>
+	int Register(const std::string& eventName, std::function<void()> func);
+
+	/// <summary>
+	/// 登録した関数を登録解除する。
+	/// </summary>
+	/// <param name="handle">登録した関数の管理番号</param>
+	void UnRegister(int handle);
+
+private:
+	std::list<EventData> m_eventDatas;
+	int m_newHandle = 0;
+};
