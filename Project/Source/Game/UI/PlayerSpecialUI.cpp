2519888f81de541a03782a5126c06c990e9369d4
Tiger5286 <toratiger5286@gmail.com>
2026-07-10 13:51:33 +0900

    定数化、コメント追加

--- Project/Source/Game/UI/PlayerSpecialUI.cpp
+++ Project/Source/Game/UI/PlayerSpecialUI.cpp
@@ -9,6 +9,9 @@ namespace
 	constexpr int kGraphMinX = 60;
 	constexpr int kGraphMaxX = 739;
 
+	constexpr int kOffsetX = 30;
+	constexpr int kOffsetY = 80;
+
 	enum class Graph
 	{
 		Frame,
@@ -55,20 +58,18 @@ void PlayerSpecialUI::Draw()
 	int w, h;
 	GetGraphSize(m_handles[static_cast<int>(Graph::Frame)], &w, &h);
 	// 枠を描画
-	int x = 30 + w / 2 * kGraphScale;
-	int y = 80 + h / 2 * kGraphScale;
+	int x = kOffsetX + w / 2 * kGraphScale;
+	int y = kOffsetY + h / 2 * kGraphScale;
 	DrawRotaGraph(x, y, kGraphScale, 0.0, m_handles[static_cast<int>(Graph::Frame)], true);
 	// 本体を描画
 	// ゲージの中身の長さを計算
 	int gaugeLen = w - ((w - kGraphMaxX) + kGraphMinX);
-	// プレイヤーのHPの割合を計算
+	// 必殺技チャージの割合を計算
 	auto player = m_pPlayer.lock();
 	float rate = static_cast<float>(player->GetSpecialCharge()) / static_cast<float>(player->kMaxSpecialCharge);
-	// 現在のHPバーの長さを計算
+	// 現在のゲージの長さを計算
 	int width = rate * gaugeLen;
 	int handle = m_handles[static_cast<int>(Graph::Main)];
-	// HPバーを描画
+	// ゲージを描画
 	DrawRectRotaGraph(x - (gaugeLen - width) * kGraphScale * 0.5f, y, kGraphMinX, 0, width, h, kGraphScale, 0.0, handle, true);
-	
-
 }
