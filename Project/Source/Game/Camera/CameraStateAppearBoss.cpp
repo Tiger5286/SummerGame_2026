bedaf663c3eda10992c9a99ca87cc7068c620e5e
Tiger5286 <toratiger5286@gmail.com>
2026-07-23 18:18:08 +0900

    ボス出現時のカメラの揺れ実装

--- Project/Source/Game/Camera/CameraStateAppearBoss.cpp
+++ Project/Source/Game/Camera/CameraStateAppearBoss.cpp
@@ -3,20 +3,41 @@
 
 #include "CameraStateFree.h"
 
+namespace
+{
+	const Vector3 kPos = Vector3(3100, 130, -6300);
+	const Vector3 kTargetPos = Vector3(3100, 0, -6800);
+
+	constexpr int kStartShakeFrame = 100;
+	constexpr int kShakeFrame = 120;
+	constexpr float kShakePower = 5.0f;
+
+	constexpr int kStateEndFrame = 270;
+}
+
 void CameraStateAppearBoss::Enter(std::weak_ptr<Camera> pCamera)
 {
 	m_pCamera = pCamera;
 	auto camera = m_pCamera.lock();
-	camera->m_pos = Vector3(3100, 130, -6300);
-	camera->m_targetPos = Vector3(3100, 0, -6800);
-	camera->m_calcPos = Vector3(3100, 130, -6300);
-	camera->m_calcTargetPos = Vector3(3100, 0, -6800);
+	camera->m_pos = kPos;
+	camera->m_targetPos = kTargetPos;
+	camera->m_calcPos = kPos;
+	camera->m_calcTargetPos = kTargetPos;
 }
 
 void CameraStateAppearBoss::Update()
 {
 	m_frame++;
-	if (m_frame > 270)
+	auto camera = m_pCamera.lock();
+
+	// カメラを揺らす
+	if (m_frame == kStartShakeFrame)
+	{
+		camera->SetShake(kShakeFrame, kShakePower);
+	}
+
+	// 一定時間たったら戻す
+	if (m_frame > kStateEndFrame)
 	{
 		ChangeState(std::make_shared<CameraStateFree>());
 		return;
