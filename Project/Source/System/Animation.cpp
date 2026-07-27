df29abb6e4f0c4be80dabc2dcd51b1c401d7a2bc
Tiger5286 <toratiger5286@gmail.com>
2026-07-27 16:00:19 +0900

    Animationクラス作成

---
+++ Project/Source/System/Animation.cpp
@@ -0,0 +1,80 @@
+﻿#include "Animation.h"
+#include "DxLib.h"
+#include <cassert>
+
+void Animation::Init(int modelHandle, std::wstring animName)
+{
+	m_modelHandle = modelHandle;
+	m_animIndex = MV1GetAnimIndex(m_modelHandle, animName.c_str());
+	assert(m_animIndex != -1 && "アニメーション名が不正です");
+}
+
+void Animation::Update()
+{
+	// 初期化、再生チェック
+	assert(m_modelHandle != -1 && "アニメーションが初期化されていません");
+	assert(m_attachIndex != -1 && "アニメーションが再生されていません");
+
+	// アニメーションを進行させる
+	MV1SetAttachAnimTime(m_modelHandle, m_attachIndex, m_animTime);
+	m_animTime += m_animSpeed;
+
+	// アニメーションの最大時間を取得
+	const float totalTime = MV1GetAttachAnimTotalTime(m_modelHandle, m_attachIndex);
+
+	// ループする場合の処理
+	if (m_isLoop)
+	{
+		// 最大時間を越したら戻す
+		while (totalTime > m_animTime)
+		{
+			m_animTime -= totalTime;
+		}
+	}
+	else	// ループしない場合
+	{
+		// 最大時間を越したら固定
+		if (totalTime > m_animTime)
+		{
+			m_animTime = totalTime;
+		}
+	}
+}
+
+void Animation::Start(bool isLoop, float animSpeed)
+{
+	// 初期化チェック
+	assert(m_modelHandle != -1 && "アニメーションが初期化されていません");
+
+	// ループ設定を適用
+	m_isLoop = isLoop;
+	// アニメーション速度を適用
+	m_animSpeed = animSpeed;
+
+	// アタッチされていたアニメーションをデタッチする
+	MV1DetachAnim(m_modelHandle, m_animIndex);
+	// アニメーションをアタッチ
+	MV1AttachAnim(m_modelHandle, m_animIndex);
+}
+
+void Animation::SetBlendRate(float rate)
+{
+	// アニメーションのブレンド率を設定
+	MV1SetAttachAnimBlendRate(m_modelHandle, m_attachIndex, rate);
+}
+
+bool Animation::IsEnd()
+{
+	// ループする場合はfalse
+	if (m_isLoop) return false;
+
+	// アニメーションの最大時間を取得
+	const float totalTime = MV1GetAttachAnimTotalTime(m_modelHandle, m_attachIndex);
+	// アニメーションが最大まで到達しているならtrue
+	if (m_animTime >= totalTime)
+	{
+		return true;
+	}
+	// アニメーションが最大まで到達していないならfalse
+	return false;
+}
---
+++ Project/Source/System/Animation.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include <string>
+
+class Animation
+{
+public:
+	static constexpr float kDefaultAnimSpeed = 0.5f;
+
+	Animation() = default;
+	~Animation() = default;
+
+	/// <summary>
+	/// アニメーションの初期化処理
+	/// </summary>
+	/// <param name="modelHandle">モデルのハンドル</param>
+	/// <param name="animName">アニメーション名</param>
+	void Init(int modelHandle, std::wstring animName);
+
+	/// <summary>
+	/// アニメーションの更新処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// アニメーションを再生開始する
+	/// </summary>
+	/// <param name="isLoop">ループするかどうか</param>
+	/// <param name="animSpeed">アニメーションの再生速度</param>
+	void Start(bool isLoop, float animSpeed = kDefaultAnimSpeed);
+
+	/// <summary>
+	/// アニメーションのブレンド率を設定する
+	/// </summary>
+	/// <param name="rate">ブレンド率(0.0 ~ 1.0)</param>
+	void SetBlendRate(float rate);
+
+	/// <summary>
+	/// アニメーションが終わったかどうか取得する　ループしている場合は常にfalseを返す
+	/// </summary>
+	/// <returns>true : アニメーションが終わった / false : アニメーションが終わっていない</returns>
+	bool IsEnd();
+
+private:
+	int m_modelHandle = -1;					// モデルのハンドル
+	int m_animIndex   = -1;					// アニメーションのインデックス
+	int m_attachIndex  = -1;					// 再生中のアニメーションのハンドル
+	float m_animSpeed = kDefaultAnimSpeed;	// アニメーションの再生速度
+	float m_animTime  = 0.0f;				// アニメーションの現在の時間
+
+	bool m_isLoop = false;	// アニメーションがループするかどうか
+};
+
