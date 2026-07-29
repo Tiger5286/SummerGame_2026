1ac1b12b4f3aadaf0eef051653c9a6927be199a3
Tiger5286 <toratiger5286@gmail.com>
2026-07-29 13:34:05 +0900

    ロードシーン改良

--- Project/Source/Scenes/SceneLoad.cpp
+++ Project/Source/Scenes/SceneLoad.cpp
@@ -7,9 +7,15 @@
 namespace
 {
 	constexpr int kLoadBarWidth = 800;
-	constexpr int kLoadBarHeight = 100;
+	constexpr int kLoadBarHeight = 30;
 
 	constexpr int kMinLoadFrame = 30;
+
+	constexpr int kLoadBarY = 650;
+
+	constexpr float kBackScale = 1.3f;
+	constexpr float kGearRotateSpeed = 0.05f;
+	constexpr int kGearOffset = 20;
 }
 
 SceneLoad::SceneLoad(SceneManager& sceneManager) : 
@@ -26,13 +32,16 @@ SceneLoad::~SceneLoad()
 
 void SceneLoad::Init()
 {
+	m_backHandle = LoadGraph(L"data/Graphs/StoneBrick.png");
+	m_gearHandle = LoadGraph(L"data/Graphs/gear.png");
 	m_firstLoadNum = GetASyncLoadNum();
 	FadeManager::GetInstance().SetFadeState(false);
 }
 
 void SceneLoad::End()
 {
-
+	DeleteGraph(m_backHandle);
+	DeleteGraph(m_gearHandle);
 }
 
 void SceneLoad::Update()
@@ -51,27 +60,35 @@ void SceneLoad::Update()
 		m_sceneManager.PopSceneWithFade();
 		return;
 	}
+
+	// 歯車を回す
+	m_gearAngle += kGearRotateSpeed;
 }
 
 void SceneLoad::Draw()
 {
-	// 背景を真っ黒で描画
-	DrawBox(0, 0, Game::kScreenWidth, Game::kScreenHeight, 0x000000, true);
+	DrawRotaGraph(Game::kScreenWidth / 2, Game::kScreenHeight / 2, kBackScale, 0.0, m_backHandle, false);
 
 	auto loadNum = GetASyncLoadNum();
 	// ロードのバーを描画
 	float progress = 1.0f - (static_cast<float>(loadNum) / static_cast<float>(m_firstLoadNum));
 	int x1 = Game::kScreenWidth / 2 - kLoadBarWidth / 2;
-	int y1 = Game::kScreenHeight / 2 - kLoadBarHeight / 2;
+	int y1 = kLoadBarY - kLoadBarHeight / 2;
 	int x2 = x1 + kLoadBarWidth * progress;
-	int y2 = Game::kScreenHeight / 2 + kLoadBarHeight / 2;
+	int y2 = kLoadBarY + kLoadBarHeight / 2;
 	DrawBox(x1, y1, x2, y2, 0xffff00, true);
+	DrawBox(x2, y1, Game::kScreenWidth / 2 + kLoadBarWidth / 2, y2, 0x000000, true);
 	// ロードのバーの枠を描画
 	x2 = Game::kScreenWidth / 2 + kLoadBarWidth / 2;
-	DrawBox(x1, y1, x2, y2, 0xffffff, false, 5);
+	DrawBox(x1, y1, x2, y2, 0x888888, false, 3);
 	// 割合を表示
 	y1 -= 20;
 	DrawFormatString(x1, y1, 0xffffff, L"%.0f%%", progress * 100.0f);
+
+	// 右下に歯車を描画
+	int w, h;
+	GetGraphSize(m_gearHandle, &w, &h);
+	DrawRotaGraph(Game::kScreenWidth - w / 2 - kGearOffset, Game::kScreenHeight - h / 2 - kGearOffset, 1.0, m_gearAngle, m_gearHandle, true);
 
 #ifdef _DEBUG
 	DrawString(0, 0, L"SceneLoad", 0xffffff);
--- Project/Source/Scenes/SceneLoad.h
+++ Project/Source/Scenes/SceneLoad.h
@@ -12,6 +12,10 @@ public:
     void Update() override;
     void Draw() override;
 private:
+    int m_backHandle = -1;
+    int m_gearHandle = -1;
+    float m_gearAngle = 0.0f;
+
     int m_loadEndCount = 0;
     int m_firstLoadNum = 0;
     bool m_isEndScene = false;
