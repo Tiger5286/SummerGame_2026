e0adb40e31205d3521e7667a2499b7271731ee21
Tiger5286 <toratiger5286@gmail.com>
2026-07-29 10:38:10 +0900

    Menuクラス作成

---
+++ Project/Menu.cpp
@@ -0,0 +1,122 @@
+﻿#include "Menu.h"
+#include "Singleton/Input.h"
+#include "Game.h"
+
+namespace
+{
+	constexpr int kFontSize = 30;
+	constexpr int kMargin = 50;
+
+	constexpr float kMaxScale = 1.1f;
+	constexpr float kLerpT = 0.2f;
+
+	constexpr unsigned int kTextColor = 0xffffff;
+}
+
+Menu::~Menu()
+{
+	DeleteGraph(m_uiHandle);
+	DeleteGraph(m_selectUIHandle);
+
+	DeleteFontToHandle(m_fontHandle);
+}
+
+void Menu::Init(std::vector<Funcs> actions, int x, int y)
+{
+	// 関数とそのアクション名を初期化
+	m_items.resize(actions.size());
+	for (int i = 0; i < actions.size(); i++)
+	{
+		m_items[i].name = actions[i].name;
+		m_items[i].action = actions[i].action;
+		// scaleとcolorRateは0.0で初期化
+	}
+	// 位置を初期化
+	m_x = x;
+	m_y = y;
+
+	m_uiHandle = LoadGraph(L"data/Graphs/Game/MenuUI.png");
+	m_selectUIHandle = LoadGraph(L"data/Graphs/Game/MenuUI_Select.png");
+
+	m_fontHandle = CreateFontToHandle(Game::kMainFontName, kFontSize, -1);
+}
+
+void Menu::Update()
+{
+	auto& input = Input::GetInstance();
+
+	// 上下キーでメニューの選択を変更する
+	if (input.IsTriggerd(XINPUT_BUTTON_DPAD_DOWN, true))
+	{
+		m_selectIndex++;
+		if (m_selectIndex >= m_items.size())
+		{
+			m_selectIndex = 0;
+		}
+	}
+	if (input.IsTriggerd(XINPUT_BUTTON_DPAD_UP, true))
+	{
+		m_selectIndex--;
+		if (m_selectIndex < 0)
+		{
+			m_selectIndex = m_items.size() - 1;
+		}
+	}
+	// 決定ボタンで選択中のメニューのアクションを実行する
+	if (input.IsTriggerd(XINPUT_BUTTON_A))
+	{
+		m_items[m_selectIndex].action();
+		return;
+	}
+
+	// UIの大きさをLerpで動かす
+	for (int i = 0; i < m_items.size(); i++)
+	{
+		// 大きさをスムーズに変える
+		if (m_selectIndex == i)
+		{
+			m_items[i].scale = std::lerp(m_items[i].scale, kMaxScale, kLerpT);
+		}
+		else
+		{
+			m_items[i].scale = std::lerp(m_items[i].scale, 1.0f, kLerpT);
+		}
+		// 色をスムーズに変える
+		if (m_selectIndex == i)
+		{
+			m_items[i].colorRate = std::lerp(m_items[i].colorRate, 1.0f, kLerpT);
+		}
+		else
+		{
+			m_items[i].colorRate = std::lerp(m_items[i].colorRate, 0.0f, kLerpT);
+		}
+	}
+}
+
+void Menu::Draw()
+{
+	for (int i = 0; i < m_items.size(); i++)
+	{
+		int handle = m_uiHandle;
+		if (i == m_selectIndex) handle = m_selectUIHandle;
+		auto actionName = m_items[i].name;
+
+		int origBlendMode, origBlendParam;
+		GetDrawBlendMode(&origBlendMode, &origBlendParam);
+
+		// 文字の背景の描画
+		int black = (1.0f - m_items[i].colorRate) * 255;
+		int red = m_items[i].colorRate * 255;
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, black);
+		DrawRotaGraph(Game::kScreenWidth / 2, m_y + kFontSize / 2 + (kFontSize + kMargin) * i, m_items[i].scale, 0.0, m_uiHandle, true);
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, red);
+		DrawRotaGraph(Game::kScreenWidth / 2, m_y + kFontSize / 2 + (kFontSize + kMargin) * i, m_items[i].scale, 0.0, m_selectUIHandle, true);
+		SetDrawBlendMode(origBlendMode, origBlendParam);
+
+		// 文字の描画
+		int width = GetDrawExtendStringWidthToHandle(m_items[i].scale, actionName.c_str(), actionName.size(), m_fontHandle);
+		DrawExtendStringToHandle(Game::kScreenWidth / 2 - width / 2,
+						   m_y + (kFontSize + kMargin) * i,
+						   m_items[i].scale, m_items[i].scale, actionName.c_str(), kTextColor, m_fontHandle);
+	}
+}
---
+++ Project/Menu.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include <string>
+#include <functional>
+#include <vector>
+
+class Menu
+{
+private:
+	struct Item
+	{
+		std::wstring name;
+		std::function<void()> action;
+		float scale = 0.0f;
+		float colorRate = 0.0f;
+	};
+
+public:
+	struct Funcs
+	{
+		std::wstring name;
+		std::function<void()> action;
+	};
+
+	Menu() = default;
+	~Menu();
+
+	/// <summary>
+	/// メニューの初期化
+	/// </summary>
+	/// <param name="actions">メニューのアクションの配列</param>
+	/// <param name="x">メニューの一番上の項目の真ん中座標X</param>
+	/// <param name="y">メニューの真ん中座標Y</param>
+	void Init(std::vector<Funcs> actions,int x,int y);
+	void Update();
+	void Draw();
+
+private:
+	std::vector<Item> m_items;	// メニューのアクション(関数)を格納する配列
+	int m_selectIndex = -1;			// 選択中のメニューの番号
+
+	int m_uiHandle = -1;
+	int m_selectUIHandle = -1;
+
+	int m_fontHandle = -1;
+
+	int m_x;
+	int m_y;
+};
+
